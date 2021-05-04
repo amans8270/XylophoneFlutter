0f19345b06b801d797e70a8e37b0564798d8c06a
Aman Singh <amans8270@gmail.com>
2021-05-05 04:19:46 +0530

    Xylophone sample flutter app completed

--- lib/main.dart
+++ lib/main.dart
@@ -6,48 +6,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void players(num) {
+    final player = AudioCache();
+    player.play('note$num.wav');
+  }
+
+  Expanded widgets1({int num, Color color  }) {
+    return (Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          players(num);
+        },
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Container(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    onPrimary: Colors.red[100]
-                  ),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note1.wav');
-                  },
-                  child: Text("Click me"),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.orange,
-                    onPrimary: Colors.orange[100]
-                  ),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note2.wav');
-                  },
-                  child: Text("Click me"),
-                ),
-                 ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.yellow,
-                    onPrimary: Colors.yellow[100]
-                  ),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note3.wav');
-                  },
-                  child: Text("Click me"),
-                ),
+                widgets1(num:1 ,color:Colors.red),
+                widgets1(num:2 ,color:Colors.orange),
+                widgets1(num:3 ,color:Colors.yellow),
+                widgets1(num:4 ,color:Colors.pink),
+                widgets1(num:5 ,color:Colors.blue),
+                widgets1(num:6 ,color:Colors.purple),
+                widgets1(num:7 ,color:Colors.red[100])
               ],
             ),
           ),
